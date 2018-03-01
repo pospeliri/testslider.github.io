76526a407b9fa6ac73bf0ff536983390cc85f56a
apospelov13 <small99@hotbox.ru>
2018-03-01 19:17:18 +0300

    v2

--- js/common.js
+++ js/common.js
@@ -2,17 +2,19 @@ $(function() {
 	//test__slider
 	var testSlider = $('.test__slider');
 	testSlider.owlCarousel({
-		nav: false,
-		dots: false,
-		items: 4,
-		margin: 10,
+    nav: false,
+    dots: false,
+    items: 4,
     loop: true,
+    margin: 20,
     autoWidth: true,
     autoplay: true,
-    autoplayTimeout: 1000,
-    smartSpeed: 5000,
+    //autoplayTimeout: 1000,
     autoplayHoverPause: true,
-	});
+    autoplaySpeed: 1000,
+    smartSpeed: 2000,
+
+  });
   
 
 });
